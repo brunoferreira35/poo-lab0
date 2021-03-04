83a830365d0ed428343ed98b36a7cae6f36287b3
Bruno Ferreira <200221068@estudantes.ips.pt>
2021-03-04 12:01:09 +0000

    AppStart inserida

---
+++ AppStart.java
@@ -0,0 +1,20 @@
+
+/**
+ * Escreva a descrição da classe AppStart aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class AppStart
+{
+  public static void main(String[] args){
+        Size size = Size.MEDIUM;
+        
+        System.out.println("Name          : "+size.name());
+        System.out.println("toString()    : \""+size.toString().trim());
+        System.out.println("Ordem         : "+size.ordinal());
+        System.out.println("Valor Mínimo  : "+size.getMinValue());
+        System.out.println("Valor Máximo  : "+size.getMaxValue());
+        System.out.println("Código        : \'"+size.getCode()+"\'");
+    }
+}
--- Size.java
+++ Size.java
@@ -7,39 +7,42 @@
  */
 public enum Size
 {
-    SMALL(32, 36, "Pequeno", 'S'),
-    MEDIUM(37, 44, "Médio", 'M'),
-    LARGE(45, 52, "Grande", 'L');
-    
+
+    SMALL(32, 36, "Pequeno",'S'),
+
+    MEDIUM(37, 44, "Médio",'M'),
+
+    LARGE(45, 52, "Grande",'L');
     private final String description;
     private final int minValue;
     private final int maxValue;
+
     private final char code;
-    
-    private Size(int minValue,int maxValue, String description,char code){
+    private Size(int minValue, int maxValue, String description, char code){
         this.description=description;
         this.minValue=minValue;
         this.maxValue=maxValue;
         this.code=code;
     }
-    
-    public String toSting(){
+
+    @Override
+    public String toString() {
         return this.description;
     }
-    
-    public String getDescription(){
+
+    public String getDescription() {
         return description;
     }
-    
-    public int getMinValue(){
+
+    public int getMinValue() {
         return minValue;
     }
-    
-    public int getMaxValue(){
+
+    public int getMaxValue() {
         return maxValue;
     }
-    
-    public char getCode(){
+
+    public char getCode() {
         return code;
-    }
+}
 }
